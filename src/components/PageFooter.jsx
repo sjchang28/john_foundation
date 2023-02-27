adfbe6ab507fc8ff2e88e7efd1129f5d9c8b309b
Stephen Chang <sjchang@ncsu.edu>
2023-02-26 21:31:00 -0500

    Updated Copyright

--- src/components/PageFooter.jsx
+++ src/components/PageFooter.jsx
@@ -1,14 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import "../style/App.css";
 
 const PageFooter = () => {
+	const [currentYear, setCurrentYear] = useState("2023");
+
+	const getCurrentYear = () => {
+		return new Date().getFullYear();
+	};
+
 	return (
 		<Fragment>
 			<div className="contnent footer">
 				<a href="/">
 					<span className="logo-text">John D. Chang Foundation<span className="registered-trademark-symbol">®</span> </span><br />
-					<div className="copyright-footer">Copyright © 2006 John D. Chang Foundation-All rights reserved.</div>
+					<div className="copyright-footer">Copyright © {getCurrentYear()} John D. Chang Foundation-All rights reserved.</div>
 				</a>
 				<div className="footer-right">
 					<a href="/">Home</a>
